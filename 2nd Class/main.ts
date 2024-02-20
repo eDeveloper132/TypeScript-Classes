956cf8dc29ade84fe1df1529c1f5a2d3f01fcfb3
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-02-20 18:39:58 +0500

    2nd Class

---
+++ 2nd Class/main.ts
@@ -0,0 +1,31 @@
+var message = "3rd Class";
+var message = "toto";
+var mrr;
+mrr = "maani";
+//var ko agar hum jo naam dety hen usi naam ka dubara bhi bana sakty hen and variable banany ky baad initialize ker sakty hen baad mein
+let governorit = "iu";
+governorit = "io";
+let ff;
+ff = "it";
+//let ko same name sy dubara nahi banasakty and variable banany ky baad initialize ker sakty hen baad mein
+const goversindh = "it";
+//const mein yeh sabiqa donu cheezein allow nahi hen
+let $ = "123";
+// console.log($);
+const _ = 123;
+// console.log(_);
+
+// Strong Typing
+let UserName : string = "Imran";
+let Age : number = 18;
+let Alive : boolean = true;
+if (Alive == true) 
+{
+    console.log("Welcome");    
+}
+else
+{
+    console.log("you passed away");
+}
+const array = [mrr,ff,$,_,UserName,Age,Alive];
+console.log(array);
