8d39250999c6126a202ec08a6f9be5531cee0ae2
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-03-05 20:23:47 +0500

    4rth class completed

--- 4rth Class/app.ts
+++ 4rth Class/app.ts
@@ -1,12 +1,12 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
 let Answers = []
 
     let Questions = await inquirer.prompt([
-        {name: "UserName",
-        age: "age",
-        gender: "gender"}
-    
+        {name: "age",type:"number",message:"Enter Your Age:"}
     ])
+    console.log(chalk.blue("Insha Allah , in "+(60 - Questions.age)+" years you will be 60 years old"));
+
 
 Answers.push(Questions);
 console.log(Answers);
