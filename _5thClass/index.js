574f926215dbd6c384750b1ea329e82feaf6880e
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-03-18 13:41:17 +0500

    5th Class Added

---
+++ _5thClass/index.js
@@ -0,0 +1,28 @@
+// const numbers : number = 55;
+// const numbers2 : number = 45;
+// const ForDivision : number = 10;
+// const ForPercent : number = 2;
+// const ForExponentiation : number = 2;
+// console.log(numbers + numbers2);// addition of two numbers
+// console.log(numbers - numbers2);// subtraction of two numbers
+// console.log(numbers * numbers2);// multiplication of two numbers
+// console.log(numbers / ForDivision);// division of two numbers
+// console.log(numbers % ForPercent);// remainder of two numbers
+// console.log(5 ** ForExponentiation);// exponentiation of two numbers
+// console.log(5*5*5*5);
+let a = 55;
+let b = 45;
+// console.log(a++);
+// console.log(a);
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+export {};
+// console.log(--a);//54
+// console.log(++a);//56
+// console.log(a--);
+// console.log(a + b);
+// preincreament ++55//56
+// predecreament --55//54
+// postincreament 55++//56
+// postdecreament 55--//54
