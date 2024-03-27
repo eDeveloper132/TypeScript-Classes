a8f26e5efb4b2616d463e0d5d94350063f004161
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-03-28 01:39:06 +0500

    6th Class Added

---
+++ _6thClass/index.ts
@@ -0,0 +1,30 @@
+// let a = 50;
+// let b = 50;
+// let c = 49;
+
+// if (a == 50) {
+//     console.log("a is 50");
+// } else {
+//     console.log("Value is incorrect");
+// }
+// if (a != 50 || b != 50) {
+//     console.log("Values is correct");
+    
+// } else {
+//     console.log("values is Incorrect");
+// }
+
+// if (a === 50 && b === 50 && c === 49) {
+//     console.log("All values are correct");
+    
+// } else {
+//     console.log("One or more values are incorrect");
+    
+// }
+// if (a == 49 || b == 50) {
+//     console.log("value are correct");
+    
+// } else {
+//     console.log("value are incorrect");
+    
+// }
