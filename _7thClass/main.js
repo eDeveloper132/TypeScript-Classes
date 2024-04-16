cd5bea4106bf7453483665b71b16a9eb29a4a7ea
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-04-16 16:30:33 +0500

    7thClass Added

---
+++ _7thClass/main.js
@@ -0,0 +1,26 @@
+//IF //ELSEIF //ELSE
+const Country = "Pakistan";
+if (Country == "Bangladesh") {
+    console.log("I am Bangladeshi");
+}
+else if (Country == "India") {
+    console.log("I am Indian");
+}
+else if (Country == "Pakistan") {
+    console.log("I am Pakistani");
+}
+else {
+    console.log("No one country selected");
+}
+//SimpleFunctions
+function Rehmatullah() {
+    console.log("Rehmatullah is still cooking");
+}
+//call //invoke
+Rehmatullah();
+const Azmatullah = () => {
+    console.log("Azmatullah is still cooking");
+};
+//call //invoke
+Azmatullah();
+export {};
---
+++ _7thClass/main.ts
@@ -0,0 +1,22 @@
+//IF //ELSEIF //ELSE
+const Country : String = "Pakistan";
+if (Country == "Bangladesh") {
+    console.log("I am Bangladeshi");
+} else if (Country == "India") {
+    console.log("I am Indian");
+} else if (Country == "Pakistan") {
+    console.log("I am Pakistani");
+} else {
+    console.log("No one country selected");
+}
+//SimpleFunctions
+function Rehmatullah(){
+    console.log("Rehmatullah is still cooking");
+}
+//call //invoke
+Rehmatullah();
+const Azmatullah = () => {
+    console.log("Azmatullah is still cooking");
+}
+//call //invoke
+Azmatullah();
