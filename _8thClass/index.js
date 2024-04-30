00644c753eac4f950773c7ce549998f2091d9f38
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-04-30 16:46:35 +0500

    8thClass added

--- _8thClass/index.js
+++ _8thClass/index.js
@@ -4,3 +4,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let a = "Muhammad";
 let b = "Ilyas";
 console.log(`My Name is ${a}${b}`);
+function Add(num1, num2) {
+    let a = num1 + num2;
+    console.log(a);
+    return a;
+}
+console.log(Add(5, 5));
--- _8thClass/index.ts
+++ _8thClass/index.ts
@@ -2,3 +2,18 @@
 let a : String = "Muhammad";
 let b : String = "Ilyas";
 console.log(`My Name is ${a}${b}`);
+
+function Subs ()
+{
+    let a : number = 5 - 5;
+    console.log(a);
+}
+Subs();
+function Add (num1:number,num2:number)
+{
+    let a : number = num1 + num2;
+    console.log(a);
+    return a;
+    
+}
+console.log(Add(5,5));
