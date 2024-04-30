20f77aea708cd600dbd6fb8ffdadb8bb8c2ec7a5
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-04-30 15:05:33 +0500

    gitignore changes

---
+++ _8thClass/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// How to concatinate 
+let a = "Muhammad";
+let b = "Ilyas";
+console.log(`My Name is ${a}${b}`);
---
+++ _8thClass/index.ts
@@ -0,0 +1,4 @@
+// How to concatinate 
+let a : String = "Muhammad";
+let b : String = "Ilyas";
+console.log(`My Name is ${a}${b}`);
