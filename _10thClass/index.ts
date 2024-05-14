9cdd8a946e6c3484d17de9da4921f725b5018b45
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-05-14 20:45:14 +0500

    _10thClass

--- _10thClass/index.ts
+++ _10thClass/index.ts
@@ -23,12 +23,44 @@ let teacher: any = {
 
 // Type Aliace
 
-type student = {
+type Student = {
     name: string,
     age: number
 }
 
-let student : student = {
+let student : Student = {
     name: "Rafay",
     age: 23
 }
+
+// Union Type Literal
+let Name: string | number ; // You can also add other types that you want in website 
+Name = "Rafay",360;
+
+// Only Type Literals
+let Name1: "ilyas" | 25 | true; // Only accepts that match values
+Name1 = "ilyas";
+
+// Another Example
+
+let Name2: string | number | true; // This can only accepts true not false
+Name2 = "ilyas" , 25 ;
+
+// Intersection // Means 2 or other types merge in 1 type
+type Teacher = {
+    Tname: string,
+    Tage: number
+}
+type PersonType = Student & Teacher // Means Student and Teacher merge in 1 type
+
+let Person : PersonType = {
+    name: "Ilyas",
+    age: 22,
+    Tname: "Ali Aftab",
+    Tage: 15
+}
+
+// Array
+let Array : string[] = ["Apple","Mango","Banana"];
+let FruitNames : string =  Array[0] + Array[1] + Array[2] ; 
+                        //   Apple     Mango     Banana
